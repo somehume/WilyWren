7674f1949690411aa9deaa4b4bcf9eef6b725879
Donnie Brown <dpbrownjr@gmail.com>
2022-05-08 16:25:41 -0400

    Initial commit

---
+++ venv/bin/mut.py
@@ -0,0 +1,7 @@
+#!/Users/donniebrown/Documents/TU/COSC603/Projects/TestingToolDemo/WilyWren/venv/bin/python3
+
+import sys
+from mutpy import commandline
+
+if __name__ == '__main__':
+    commandline.main(sys.argv)
